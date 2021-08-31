1e6fcadeac7aa69ded13c28a8296b3c5dd0c8803
Pedro Bennesby <pedrobennesby@gmail.com>
2021-08-31 08:19:26 -0300

    feat(front): Inicio do projeto e header finalizada

---
+++ components/Navbar.tsx
@@ -0,0 +1,78 @@
+import { Flex, Button, IconButton, Icon } from '@chakra-ui/react';
+import { useState } from 'react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
+
+export const Navbar = () => {
+  const [display, changeDisplay] = useState('none');
+  return (
+    <Flex>
+      <Flex pos="fixed" top="1rem" right="1rem"></Flex>
+      <Flex display={['none', 'none', 'flex', 'flex']}>
+        <NextLink href="/" passHref>
+          <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+            Home
+          </Button>
+        </NextLink>
+        <NextLink href="/sobre" passHref>
+          <Button as="a" variant="ghost" aria-label="Sobre" my={5} w="100%">
+            Sobre
+          </Button>
+        </NextLink>
+        <NextLink href="/contato" passHref>
+          <Button as="a" variant="ghost" aria-label="Contato" my={5} w="100%">
+            Contato
+          </Button>
+        </NextLink>
+      </Flex>
+      <IconButton
+        aria-label="Abrir Menu"
+        size="lg"
+        mr={2}
+        icon={<HamburgerIcon />}
+        display={['flex', 'flex', 'none', 'none']}
+        onClick={() => changeDisplay('flex')}
+      />
+      <Flex
+        w="100vw"
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+        display={display}
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Fechar Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => changeDisplay('none')}
+          />
+        </Flex>
+        <Flex flexDir="column" align="center">
+          <NextLink href="/" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+              Home
+            </Button>
+          </NextLink>
+          <NextLink href="/sobre" passHref>
+            <Button as="a" variant="ghost" aria-label="Sobre" my={5} w="100%">
+              Sobre
+            </Button>
+          </NextLink>
+          <NextLink href="/contato" passHref>
+            <Button as="a" variant="ghost" aria-label="Contato" my={5} w="100%">
+              Contato
+            </Button>
+          </NextLink>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
